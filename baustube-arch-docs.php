b30f9ace1ab2a136e82ebd4519e6b76bc954b061
Silvan Hagen <neverything@gmail.com>
2020-01-08 16:35:34 +0100

    Bump version to trigger Github Updater

--- baustube-arch-docs.php
+++ baustube-arch-docs.php
@@ -7,7 +7,7 @@
  * Author URI:      https://silvanhagen.com/
  * Text Domain:     baustube-arch-docs
  * Domain Path:     /languages
- * Version:         0.3.0
+ * Version:         0.4.0
  * GitHub Plugin URI: neverything/baustube-arch-docs
  *
  * @package         Neverything\Baustube_Arch_Docs
